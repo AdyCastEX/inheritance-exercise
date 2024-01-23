38f1db646d9256bb6a04afece72e816514d0b41f
Tristan John P. Girao <tristanjohn@mashupgarage.com>
2024-01-23 12:56:43 +0800

    [Feat] Ruby inheritance exercise initial commit

--- car.rb
+++ car.rb
@@ -1,4 +1,17 @@
 require "./vehicle.rb"
 
 class Car < Vehicle
+    attr_reader :nickname,
+
+    def initialize(args)
+        @nickname = args[:nickname]
+        super(args)
+    end
+
+    def spares
+        super.merge({
+            tire_size: @tire_size || "P215/65R17 98T",
+            nickname: @nickname
+        })
+    end
 end
